ab28e163cc7ef3d8f69ea200fcf22056230629a7
Cannigia Laluw <cannigialaluw@gmail.com>
2021-06-08 19:14:55 -0400

    📍ℹ️  adding info popup to markers

--- src/features/map/screen/MapScreen.js
+++ src/features/map/screen/MapScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components";
 import { LocationContext } from "../../../services/location/location.context";
@@ -8,13 +8,40 @@ import { MapSearch } from "../components/MapSearch";
 export const MapScreen = () => {
   const { location } = useContext(LocationContext);
   const { restaurants } = useContext(RestaurantsContext);
+  // console.log(location)
+  const { lat, lng, viewport } = location;
+
+  const [latDelta, setLatDelta] = useState(0); // determines the zoom level on render
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
 
   return (
     <>
       <MapSearch />
-      <Map>
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
         {restaurants.map((restaurant) => {
-          return null;
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            />
+          );
         })}
       </Map>
     </>
