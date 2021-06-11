7579e251438a8cd5d4d2a923e4c6a2ee08e335e2
Cannigia Laluw <cannigialaluw@gmail.com>
2021-06-11 00:07:29 -0400

    🙋 Adding firebase <Auth> to project

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import { StatusBar as ExpoStatsBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { MyTabs } from "./src/infrastructure/navigation/app.navigator";
-
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import * as firebase from "firebase";
 
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDw8fbwtoUzdyBnBTgeeN7vjgAdVht5Rts",
+  authDomain: "pickmealup-8443a.firebaseapp.com",
+  projectId: "pickmealup-8443a",
+  storageBucket: "pickmealup-8443a.appspot.com",
+  messagingSenderId: "549207349307",
+  appId: "1:549207349307:web:879889f124b9cce1fd641e",
+};
+
+// Initialize Firebase and make sure that firebase app only initiated once.
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("testman@mail.com", "test123")
+      .then((user) => {
+        console.log(user);
+        setIsAuthenticated(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
--- README.md
+++ README.md
@@ -18,3 +18,4 @@ The plan is to connect to Google maps API and make suggestions based of the API 
     ∙ React Navigation and gesture handlers
     ∙ React Navigation stack
     ∙ Camelize (To transform data received from json with an underscore (ex: this_data) into camel-cased (ex: thisData) and)
+    ∙ Google Firebase for user authentication
