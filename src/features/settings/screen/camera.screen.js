1c6654994536fe49cdfc1b7f2479f5196208a004
Cannigia Laluw <cannigialaluw@gmail.com>
2021-09-13 23:59:19 -0400

    storing photo in local storage

--- src/features/settings/screen/camera.screen.js
+++ src/features/settings/screen/camera.screen.js
@@ -1,19 +1,24 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useContext } from "react";
 import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { Text } from "../../../components/typography/text.component";
 import { Camera } from "expo-camera";
+
+import { AuthContext } from "../../../services/authentication/auth.context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import styled from "styled-components/native";
 
 // export const CameraScreen = () => null;
 export const CameraScreen = () => {
   const [hasPermission, setHasPermission] = useState("");
   const cameraRef = useRef();
+  const { user } = useContext(AuthContext);
 
   // camera action are outisde of React Native, so use await
   const snap = async () => {
     if (cameraRef) {
       const photo = await cameraRef.curent.takePictureAsync();
       console.log(photo);
+      AsyncStorage.setItem(` ${user.uid}-photo`, photo.uri);
     }
   };
 
